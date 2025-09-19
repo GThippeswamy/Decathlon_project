1829c12a435755160438712bf108d0129d7ed041
GThippeswamy <your_github_email@example.com>
2025-09-19 18:09:09 +0530

    hometest

---
+++ HomepageTest.java
@@ -0,0 +1,40 @@
+package DecathlonProjectTestRunner;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+import DecathlonProjectTest.Homepage;
+
+import java.time.Duration;
+
+public class HomepageTest {
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void setup() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.decathlon.in/");
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void homepageFunctionalityTest() {
+        Homepage page = new Homepage(driver);
+        page.closePopupIfVisible();
+        page.clickProducts();
+
+        page.TriblebarClick();
+        page.Logo();
+        page.clickMyStore();
+        page.clickWishlist();
+        page.clickCart();
+
+    }
+
+    @AfterClass
+    public void teardown() {
+        driver.quit();
+    }
+}
